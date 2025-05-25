4b3aff07109e77422fa965f081c4253d689e3d8b
meegle84 <22105488+meegle84@users.noreply.github.com>
2025-05-25 10:55:07 +0200

    Create oripa.rb

---
+++ Casks/oripa.rb
@@ -0,0 +1,16 @@
+cask "oripa" do
+  version "1.75"
+  sha256 "4757d3398e1bcd01f87b796e02589c53446ccd9b92418694993f0d1d50883e8b":contentReference[oaicite:11]{index=11}
+
+  url "https://github.com/oripa/oripa/releases/download/v#{version}_release/ORIPA-#{version}.dmg",
+      verified: "github.com/oripa/oripa/"
+  name "ORIPA"
+  desc "ORIPA is a drawing software dedicated to designing the crease patterns of origami. The unique feature of ORIPA is calculation of the folded shape from the pattern."
+  homepage "https://github.com/oripa/oripa"
+
+  app "ORIPA.app"
+
+  zap trash: [
+    "~/Library/Application Support/oripa",
+  ]
+end
